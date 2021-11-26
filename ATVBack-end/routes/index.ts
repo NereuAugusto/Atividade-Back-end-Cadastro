c4af0171c6eede82ae503a3cdc3a23d3330fc2f9
Nereu Augusto <78824974+NereuAugusto@users.noreply.github.com>
2021-11-26 13:21:00 -0300

    Add files via upload

---
+++ ATVBack-end/routes/index.ts
@@ -0,0 +1,72 @@
+﻿import app = require("teem");
+
+//**********************************************************************************
+// Se por acaso ocorrer algum problema de conexão, autenticação com o MySQL,
+// por favor, execute este código abaixo no MySQL e tente novamente!
+//
+// ALTER USER 'USUÁRIO'@'localhost' IDENTIFIED WITH mysql_native_password BY 'SENHA';
+//
+// * Assumindo que o usuário seja root e a senha root, o comando ficaria assim:
+//
+// ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root';
+//
+//**********************************************************************************
+
+class IndexRoute {
+	public async index(req: app.Request, res: app.Response) {
+		res.render("index/index");
+	}
+
+	@app.http.post()
+	public async criarLivro(req: app.Request, res: app.Response) {
+		// Os dados enviados via POST ficam dentro de req.body
+		let livro = req.body;
+
+		// É sempre muito importante validar os dados do lado do servidor,
+		// mesmo que eles tenham sido validados do lado do cliente!!!
+		if (!livro) {
+			res.status(400);
+			res.json("Dados inválidos");
+			return;
+		}
+
+		if (!livro.titulo) {
+			res.status(400);
+			res.json("Titulo inválido");
+			return;
+		}
+
+		livro.ano = parseInt(livro.ano);
+		if (isNaN(livro.ano)) {
+			res.status(400);
+			res.json("Ano inválido");
+			return;
+		}
+
+		if (!livro.autor) {
+			res.status(400);
+			res.json("Autor inválido");
+			return;
+		}
+
+		livro.paginas = parseInt(livro.paginas);
+		if (isNaN(livro.paginas)) {
+			res.status(400);
+			res.json("Número de páginas inválido");
+			return;
+		}
+
+		await app.sql.connect(async (sql) => {
+
+			// Todas os comandos SQL devem ser executados aqui dentro do app.sql.connect().
+
+			// As interrogações serão substituídas pelos valores passados ao final, na ordem passada.
+			await sql.query("INSERT INTO livro (titulo, ano, autor, paginas) VALUES (?, ?, ?, ?)", [livro.titulo, livro.ano, livro.autor, livro.paginas]);
+
+		});
+
+		res.json(true);
+	}
+}
+
+export = IndexRoute;
