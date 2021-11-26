56a2cdd975a9f209ac43540363201daa0a018d78
Nereu Augusto <78824974+NereuAugusto@users.noreply.github.com>
2021-11-26 13:21:35 -0300

    Add files via upload

---
+++ ATVBack-end2/routes/index.ts
@@ -0,0 +1,35 @@
+﻿import app = require("teem");
+
+//**********************************************************************************
+// Se por acaso ocorrer algum problema de conexão, autenticação com o MySQL,
+// por favor, execute este código abaixo no MySQL e tente novamente!
+//
+// ALTER USER 'USUÁRIO'@'localhost' IDENTIFIED WITH mysql_native_password BY 'SENHA';
+//
+// * Assumindo que o usuário seja root e a senha root, o comando ficaria assim:
+//
+// ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root';
+//
+//**********************************************************************************
+
+class IndexRoute {
+	public async index(req: app.Request, res: app.Response) {
+		let livro: any[];
+
+		await app.sql.connect(async (sql) => {
+
+			// Todas os comandos SQL devem ser executados aqui dentro do app.sql.connect().
+
+			livro = await sql.query("SELECT id, titulo, ano, autor, paginas FROM livro");
+
+		});
+
+		let opcoes = {
+			livro: livro
+		};
+
+		res.render("index/index", opcoes);
+	}
+}
+
+export = IndexRoute;
